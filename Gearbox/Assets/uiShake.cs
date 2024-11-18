2d1e49858ff2dffccbcb9726ff7f7c388a84ff69
cacosta07 <christian.acosta@student.horizonhonors.org>
2024-11-18 08:46:01 -0700

    texture update

--- Gearbox/Assets/uiShake.cs
+++ Gearbox/Assets/uiShake.cs
@@ -6,16 +6,16 @@ public class uiShake : MonoBehaviour
 {
 
     public MovementHandler movementHandler;
+    [SerializeField] Vector3 FixedPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-       Vector3 position = transform.position;
-        transform.position = new Vector3(position[0] + Mathf.Sin(movementHandler.velocityMagnitude), position[1] + Mathf.Sin(movementHandler.velocityMagnitude), transform.position.z);
+        Vector3 position = transform.position;
+        transform.position = new Vector3(FixedPosition[0] + Mathf.Sin(movementHandler.velocityMagnitude), FixedPosition[1], transform.position.z);
     }
 }
